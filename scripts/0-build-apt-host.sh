b51bffa132f246cdc4644a21f19a2ba2d11f4329
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-08-09 14:01:44 -0700

    full upgrade!

--- scripts/0-build-apt-host.sh
+++ scripts/0-build-apt-host.sh
@@ -8,7 +8,7 @@ echo "** Build "apt" Host ** "
 echo "Installing host dependencies"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -qq
-sudo apt-get upgrade -qqy
+sudo apt-get full-upgrade -qqy
 sudo apt-get install -qqy avahi-utils build-essential git time
 
 echo "..Installing Homebrew"
